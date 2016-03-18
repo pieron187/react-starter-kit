6493f672d6ae34ea320e26c7d2f3e6cbcc8ea0d5
pieron187 <pieron187@op.pl>
2016-03-18 11:30:31 +0000

    Test commit 1
    sdsdsds

--- jsx/script.js
+++ jsx/script.js
@@ -1,16 +1,15 @@
 var Photo = React.createClass({
 
-  render: function() {
-    return (
-      <div className='photo'>
-        <img src={this.props.src} />
-        <span>{this.props.caption}</span>
-      </div>
-    );
-  }
+	render: function() {
+		return (
+			<div className='photo'>
+				<img src={this.props.src} />
+				<span>{this.props.caption}</span>
+			</div>
+		);
+	}
 });
 
 
 React.render(<Photo src='http://tinyurl.com/lkevsb9' caption='Bla bla!' />, document.querySelectorAll(".testClass")[0]);
-
 React.render(<Photo src='http://tinyurl.com/lkevsb9' caption='Hong Kong!' />, document.querySelector(".testClass2"));
