684375045733673292bbf3ed0e7d14b71e86a74b
pieron187 <pieron187@op.pl>
2016-03-18 13:03:55 +0000

    Adding to myReact Photo component
    * styles
    * script.js
    * newer version of react in index

--- jsx/script.js
+++ jsx/script.js
@@ -1,15 +1,56 @@
-var Photo = React.createClass({
+(function() {
+	var myReact = {};
 
-	render: function() {
-		return (
-			<div className='photo'>
-				<img src={this.props.src} />
-				<span>{this.props.caption}</span>
-			</div>
-		);
-	}
-});
+	myReact.Photo = React.createClass({
+
+		getInitialState: function() {
+			return {
+				liked: false,
+				color: 'red'
+			};
+		},
+
+		toggleLiked: function() {
+			this.setState({
+				liked: !this.state.liked,
+				color: this.state.color === 'red' ? 'blue' : 'red'
+			});
+		},
 
 
-React.render(<Photo src='http://tinyurl.com/lkevsb9' caption='Bla bla!' />, document.querySelectorAll(".testClass")[0]);
-React.render(<Photo src='http://tinyurl.com/lkevsb9' caption='Hong Kong!' />, document.querySelector(".testClass2"));
+		render: function() {
+			var buttonClass = this.state.liked ? 'active' : '';
+
+			return (
+				<div className='photo'>
+					<img src={this.props.src} className={this.state.color} />
+					<div className='bar'>
+						<button onClick={this.toggleLiked} className={buttonClass}>♥</button>
+						<span>{this.props.caption}</span>
+					</div>
+				</div>
+			);
+		}
+	});
+
+
+	myReact.setEvents = function() {
+		document.getElementsByTagName('body')[0].addEventListener('click', function( event ) {
+			console.log('click count: ' + event.detail);
+			console.log('event.target.innerHTML: ' + event.target.innerHTML);
+		}, false);
+	};
+
+
+	myReact.init = function() {
+		React.render(<myReact.Photo src='http://tinyurl.com/lkevsb9' caption='Bla bla!' />, document.querySelectorAll('.testClass')[0]);
+		React.render(<myReact.Photo src='http://tinyurl.com/lkevsb9' caption='Hong Kong!' />, document.querySelector('.testClass2'));
+
+		myReact.setEvents();
+
+	};
+
+
+	myReact.init();
+
+}());
